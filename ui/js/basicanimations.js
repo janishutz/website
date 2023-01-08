c0ce4420909660f740e20e1ca5088b5309a2fe1d
janis <jahu04@outlook.com>
2023-01-08 11:09:12 +0100

    fixes

--- ui/js/basicanimations.js
+++ ui/js/basicanimations.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
 $('#ContentToInclude').load('/menu.html');
 $('#Footer').load('/footer.html');
-    $('.linkbutton').mouseenter(function() {
-        $(this).stop()
-        $(this).animate({'border-radius': '0px', 'background-color': 'darkblue'}, 200)
-    })
-    $('.linkbutton').mouseleave(function() {
-        $(this).stop()
-        $(this).animate({'border-radius': '20px', 'background-color': 'rgba(0,0,0,0)'}, 200)
-    })
-    $('.textlink').mouseenter(function() {
-        $(this).stop()
-        $(this).animate({'color': 'darkblue', 'font-size': '120%'}, 200)
-    })
-    $('.textlink').mouseleave(function() {
-        $(this).stop()
-        $(this).animate({'color': 'black', 'font-size': '100%'}, 200)
-    })
-    setTimeout(highlightLocation, 200)
+    $( '.linkbutton').mouseenter(function() {
+        $( this ).stop();
+        $( this ).animate( { 'border-radius': '0px', 'background-color': 'darkblue' }, 200 );
+    });
+    $( '.linkbutton').mouseleave(function() {
+        $( this ).stop();
+        $( this ).animate( { 'border-radius': '20px', 'background-color': 'rgba(0,0,0,0)' }, 200 );
+    });
+    $( '.textlink' ).mouseenter(function() {
+        $( this ).stop();
+        $(this ).animate( { 'color': 'darkblue', 'font-size': '120%' }, 200 );
+    });
+    $( '.textlink' ).mouseleave(function() {
+        $( this ).stop();
+        $( this ).animate( { 'color': 'black', 'font-size': '100%' }, 200);
+    });
+    setTimeout( highlightLocation, 200 );
 });
 
 function highlightLocation () {
-    let pagename = $(location).attr('pathname')
+    let pagename = $(location).attr( 'pathname' );
     if ( pagename.slice(0, 8) == '/project' ) {
-        $('#projects').css('background-color', 'darkblue');
-    } else if ( pagename.slice(0, 6) == '/about' || pagename.slice(0, 14) == '/privacypolicy' ) {
-        $('#about').css('background-color', 'darkblue');
-    } else if ( pagename.slice(0, 6) == '/' || pagename == '/index.html' ) {
-        $('#home').css('background-color', 'darkblue');
-    } else if ( pagename.slice(0, 6) == '/support' ) {
-        $('#support').css('background-color', 'darkblue');
-    } else if ( pagename.slice(0, 6) == '/services' ) {
-        $('#services').css('background-color', 'darkblue');
+        $( '#projects' ).css( 'background-color', 'darkblue' );
+    } else if ( pagename.slice( 0, 6 ) == '/about' || pagename.slice( 0, 14 ) == '/privacypolicy' ) {
+        $( '#about' ).css( 'background-color', 'darkblue' );
+    } else if ( pagename.slice( 0, 6 ) == '/' || pagename == '/index.html' ) {
+        $( '#home' ).css( 'background-color', 'darkblue' );
+    } else if ( pagename.slice( 0, 8 ) == '/support' ) {
+        $( '#support' ).css( 'background-color', 'darkblue' );
+    } else if ( pagename.slice( 0, 9 ) == '/services' ) {
+        $( '#services' ).css( 'background-color', 'darkblue') ;
     };
 }
